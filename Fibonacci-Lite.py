8242931979311752a5cc5a56d1baa4b2e673ac5c
Mony Lin <66572898+monybun@users.noreply.github.com>
2021-01-19 17:28:55 +0000

    Update Fibonacci-Lite.py

--- Fibonacci-Lite.py
+++ Fibonacci-Lite.py
@@ -1,8 +1,6 @@
 def fibonacci_lite(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
+    if n == 0 or n == 1:
+        return n
     
     return fibonacci_lite(n-1) + fibonacci_lite(n-2)
     
