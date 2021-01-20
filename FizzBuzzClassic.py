d3512e1ad60ee7610d13c66cd5a8ba327d9f5367
Mony Lin <66572898+monybun@users.noreply.github.com>
2021-01-20 00:27:02 +0000

    Update FizzBuzzClassic.py

--- FizzBuzzClassic.py
+++ FizzBuzzClassic.py
@@ -1,18 +1,17 @@
-#Python 3
-def fizzBuzz(n):
-    for i in range(n):
-        if i == 0: continue
-        if i % 3 == 0 and i % 5 == 0:
-            print("fizzbuzz")
-            continue
-        elif i % 3 == 0:
-            print("fizz")
-            continue
-        elif i % 5 == 0:
-            print("buzz")
-            continue
-        print(i)
-if __name__ == '__main__':
-    n = int(input().strip())
+def fizzBuzz(n):
+    for i in range(n+1):
+        if i == 0: continue
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+            continue
+        elif i % 3 == 0:
+            print("Fizz")
+            continue
+        elif i % 5 == 0:
+            print("Buzz")
+            continue
+        print(i)
 
-    fizzBuzz(n)
+n = int(input().strip())
+
+fizzBuzz(n)
